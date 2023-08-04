0da7c99163983ed1883b9f0994caaead4974572c
Brandon Roberts <robertsbt@gmail.com>
2023-08-04 16:38:19 -0500

    build: convert commonjs packages to esm

--- packages/vite-plugin-angular/src/lib/utils/devkit.ts
+++ packages/vite-plugin-angular/src/lib/utils/devkit.ts
@@ -1,16 +1,13 @@
-import { requiresLinking } from '@angular-devkit/build-angular/src/tools/babel/webpack-loader';
-import angularApplicationPreset from '@angular-devkit/build-angular/src/tools/babel/presets/application';
+import { requiresLinking } from '@angular-devkit/build-angular/src/tools/babel/webpack-loader.js';
+import angularApplicationPreset from '@angular-devkit/build-angular/src/tools/babel/presets/application.js';
 
-import { createJitResourceTransformer } from '@angular-devkit/build-angular/src/tools/esbuild/angular/jit-resource-transformer';
-import { CompilerPluginOptions } from '@angular-devkit/build-angular/src/tools/esbuild/angular/compiler-plugin';
-import { JavaScriptTransformer } from '@angular-devkit/build-angular/src/tools/esbuild/javascript-transformer';
-import { SourceFileCache } from '@angular-devkit/build-angular/src/tools/esbuild/angular/compiler-plugin';
-
-import { loadEsmModule } from '@angular-devkit/build-angular/src/utils/load-esm';
+import { createJitResourceTransformer } from '@angular-devkit/build-angular/src/tools/esbuild/angular/jit-resource-transformer.js';
+import { CompilerPluginOptions } from '@angular-devkit/build-angular/src/tools/esbuild/angular/compiler-plugin.js';
+import { JavaScriptTransformer } from '@angular-devkit/build-angular/src/tools/esbuild/javascript-transformer.js';
+import { SourceFileCache } from '@angular-devkit/build-angular/src/tools/esbuild/angular/compiler-plugin.js';
 
 export {
   requiresLinking,
-  loadEsmModule,
   angularApplicationPreset,
   createJitResourceTransformer,
   CompilerPluginOptions,
